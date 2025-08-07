da98c8b75c794b631d5e5451c46794324fd508fc
greensnapback0229 <smdmim@gmail.com>
2025-08-07 17:42:12 +0900

    feat: add goroutine-based chunk splitting

--- internal/chunker/chunker.go
+++ internal/chunker/chunker.go
@@ -1,12 +1,58 @@
 package chunker
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
 
-const DefaultChunkSize = 5 * 1024 * 1024 // 5MB
+const chunkSize = 4 * 1024 * 1024 // 4MB
 
-func SplitFile(path string) ([]string, error) {
-	fmt.Printf("Splitting file: %s into %dMB chunks...\n", path, DefaultChunkSize/1024/1024)
+/*
+*
 
-	// TODO: Actually read and split file into chunks
-	return []string{"chunk1", "chunk2"}, nil
+	split chunk from single file
+*/
+func splitFileToChunks(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, chunkSize)
+	chunkNum := 0
+
+	for {
+		n, err := file.Read(buf)
+		if n > 0 {
+			chunkFileName := fmt.Sprintf("%s_chunk%d", filepath.Base(filePath), chunkNum)
+			err := writeChunk(chunkFileName, buf[:n])
+			if err != nil {
+				return fmt.Errorf("failed to write chunk %d: %w", chunkNum, err)
+			}
+			fmt.Printf("Wrote chunk %d: %s (%d bytes)\n", chunkNum, chunkFileName, n)
+			chunkNum++
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("read error: %w", err)
+		}
+	}
+	return nil
+}
+
+func writeChunk(filename string, data []byte) error {
+	//Todo: write chunk to s3
+}
+
+func main() {
+	filePath := "video.mp4" // 분할할 파일명으로 변경하세요
+	err := splitFileToChunks(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
